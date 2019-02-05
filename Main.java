43ae3359525c18c44ccd5e7cafa1dcc61488eaa7
Ed <eisajanyan@gmail.com>
2019-02-05 01:34:43 -0800

    setup

---
+++ Main.java
@@ -0,0 +1,13 @@
+import WarmUp
+        .*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[] strArgs = new String[0];
+        
+        .main(strArgs);
+        
+    }
+    
+}
