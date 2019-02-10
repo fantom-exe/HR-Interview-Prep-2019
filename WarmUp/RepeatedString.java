4e27fdbdec4bb91af6bb2347949f49973514b80e
Edward <eisajanyan@gmail.com>
2019-02-09 17:28:05 -0800

    repeatedString IP

--- WarmUp/RepeatedString.java
+++ WarmUp/RepeatedString.java
@@ -13,9 +13,33 @@ public class RepeatedString {
 	
 	// Complete the repeatedString function below.
 	static long repeatedString(String s, long n) {
-		long repeats = 0;
+		final long  LENGTH_OF_S = s.length(),
+					N_MOD_A     = n % s.length();
+		
+		// count number of 'a' in string s
+		int numOfa = 0;
+		
+		for(int i = 0; i < LENGTH_OF_S; i++) {
+			if(s.charAt(i) == 'a') {
+				numOfa++;
+			}
+		}
+		
+		final long NUM_OF_A_IN_S = numOfa;
+		
+		long repeats = 0; // 
+		
+		// calculate repeats of 'a'
+		if(N_MOD_A == 0) {
+			repeats = n * NUM_OF_A_IN_S / LENGTH_OF_S;
+		}
+		else {
+			// n - n % LENGTH_OF_S
+			
+		}
 		
 		
+		System.out.println(9 * 2/3);
 		
 		return repeats;
 	}
