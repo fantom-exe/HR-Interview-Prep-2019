8dbdf026b550cc8ce8a9a01458e0b9e5225ad1ed
Edward <eisajanyan@gmail.com>
2019-02-19 01:36:55 -0800

    NewYearChaos setup

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String[] strArgs = new String[0];
     
-        ArraysLeftRotation.main(strArgs);
+        NewYearChaos.main(strArgs);
         
     }
     
