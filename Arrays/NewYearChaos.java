39c1bd6c5fb0a15174b481ca34f3ac7cc8f20bb6
Edward <eisajanyan@gmail.com>
2019-02-24 03:18:02 -0800

    newYearChaos final test

--- Arrays/NewYearChaos.java
+++ Arrays/NewYearChaos.java
@@ -16,46 +16,46 @@ public class NewYearChaos {
 	
 	// Complete the minimumBribes function below.
 	static void minimumBribes(int[] q) {
-		int person, nextPerson, tempBribes, totalBribes = 0;
+		int person, nextPerson, tempBribes = 0, totalBribes = 0;
+		boolean wasChaotic = false;
 		
 		for(int i = 0; i < q.length-1; i++) { // queue counter
 			person = q[i];
-			tempBribes = 0; // reset bribe counter
 			
 			for(int j = i+1; j < q.length; j++) { // bribe counter
 				nextPerson = q[j];
+				
+				if(person > nextPerson) { // bribe occurred
+					tempBribes++;
+				}
+				
+				if(tempBribes > 2) { // more than 2 bribes
+					wasChaotic = true;
+					tempBribes = 0; // reset bribe counter
+					break;
+				}
 				
 				System.out.println("person: " + person);
 				System.out.println("nextPerson: " + nextPerson);
 				
 				System.out.println("**tempBribes: " + tempBribes);
 				System.out.println("**totalBribes: " + totalBribes);
+				System.out.println("wasChaotic: " + wasChaotic);
 				System.out.println();
-				
-				if(tempBribes > 2) { // more than 2 bribes
-					chaotic = true;
-					break;
-				}
-				else if(person > nextPerson) { // bribe occurred
-					tempBribes++;
-					
-					if(person-1 == nextPerson) { // for efficiency
-						break;
-					}
-				}
 				
 			} // END for - bribe counter
 			
-			if(!chaotic) {
-				totalBribes += tempBribes;
-			}
-			else {
-				break;
-			}
+			totalBribes += tempBribes;
+			tempBribes = 0; // reset bribe counter
 			
 		} // END for
 		
-		minimumBribes += totalBribes;
+		if(!wasChaotic) {
+			minimumBribes += totalBribes;
+		}
+		else {
+			chaotic = true;
+		}
 	}
 	
 	private static final Scanner scanner = new Scanner(System.in);
