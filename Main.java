723be0ccbf64e2a7880526c8e97cd5d66f92a7a7
Edward <eisajanyan@gmail.com>
2019-02-28 02:57:25 -0800

    MinimumSwaps2 setup

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String[] strArgs = new String[0];
     
-        NewYearChaos.main(strArgs);
+        MinimumSwaps2.main(strArgs);
         
     }
     
